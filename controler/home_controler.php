f9a15c042e777e5023362caf5bcd13212085ad88
vagrant <vagrant@vagrant.vm>
2018-12-10 15:07:52 +0000

    [MVC] Structure mvc fonctionnelle et affichage de la page d'accueil ok

---
+++ controler/home_controler.php
@@ -0,0 +1,15 @@
+<?php
+// switch qui définit l'action à effectuer
+switch ($action) {
+
+    case 'display':
+        require 'model/home_model.php';
+        break;
+
+    default: // conportement par défaut quand il n'y a pas de cas reconnu par le switch
+        
+        break;
+ }
+
+// Rendu du block twig avec les données récupérées dans le fichier home_model.php
+echo $twig->render('home.twig', array());
