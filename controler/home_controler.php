1ea2c0404b1a59e7fd8edb9b46ed162c2254f80c
vagrant <vagrant@vagrant.vm>
2018-12-19 20:30:01 +0000

    [PHP][Controler] Fonctions du controler home corrigés

--- controler/home_controler.php
+++ controler/home_controler.php
@@ -23,14 +23,14 @@ switch ($action) {
     break;
 
     default: //Affichage de la page 404
-    echo $twig->render('404.twig', array());
+        echo $twig->render('404.twig', array());
     break;
  }
 
 //Fonction d'upload du fichier
 function upload(){
 
-    //global $bdd;
+    global $bdd;
     global $twig;
 
     //Récupération des données du formulaire
@@ -47,91 +47,108 @@ function upload(){
     $mailDesti = $_POST['emailDestinataire'];   //Mail du destinataire
     $expediteur = $_POST['nom'];
 
-    uploadFile($name, $type, $size, $tmp_name, $error);   //Upload du fichier sur le serveur
-    updateDb($name, $extension_upload, $message, $id, $size, $date_up, $mailExpe, $expediteur, $mailDesti);     //Mise à jour de la base de données
-    envoiMail($number, $emailExpediteur, $emailDestinataire, $message, $size, $fichier, $submit);    //Envoie du mail
+    $upvars = uploadFile($name, $type, $size, $tmp_name, $error, $maxsize);   //Upload du fichier sur le serveur
+    
+    //Variabes à récupérer de la fonction uploadFile et à passer aux fonctions suivantes
+    $extension_upload = $upvars[0]; 
+    $id = $upvars[1];
+    $number = $upvars[2];
+    $info = $upvars[3];
+    $erreur = $upvars[4];
+
+    //echo 'variables bdd : '.$name." : ".$extension_upload." : ".$message." : ".$id." : ".$size." : ".$date_up." : ".$mailExpe." : ".$expediteur." : ".$mailDesti;
+    updateDb($name, $extension_upload, $message, $id, $size,  $date_up, $mailExpe, $expediteur, $mailDesti);     //Mise à jour de la base de données
+    envoiMail($info, $number, $mailExpe, $mailDesti, $message, $size, $name);    //Envoie du mail
 
     //affichage de la page d'information
     echo $twig->render('info.twig', array('info'=>$info, 'erreur'=>$erreur));
 
-    function uploadFile($name, $type, $size, $tmp_name, $error){
-        //Vérification du transfert
-        if ($error > 0){
-            $erreur = "Erreur lors du transfert";
-        } 
+  
+}
+
+function uploadFile($name, $type, $size, $tmp_name, $error, $maxsize){
+    //Vérification du transfert
+    if ($error > 0){
+        $erreur = "Erreur lors du transfert";
+    } 
+
+    //Vérification de la taille du fichier
+    if ($size > $maxsize){
+        $erreur = "Le fichier est trop gros. La taille maximum est de 2Go";
+    } 
+
+    $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png', 'ico' ,'mp3', 'm4a', 'ogg', 'wav', 'mp4', 'm4v', 'mov', 'wmv', 'zip', 'rar', 'avi', 'mpg', 'ogv', '3gp', '3g2', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'pps', 'ppsx', 'odt', 'xls', 'xlsx', 'psd' );
+    //1. strrchr renvoie l'extension avec le point (« . »).
+    //2. substr(chaine,1) ignore le premier caractère de chaine.
+    //3. strtolower met l'extension en minuscules.
+    $extension_upload = strtolower(  substr(  strrchr($_FILES['icone']['name'], '.')  ,1)  );
     
-        //Vérification de la taille du fichier
-        if ($size > $maxsize){
-            $erreur = "Le fichier est trop gros. La taille maximum est de 2Go";
-        } 
+    //Vérification de l'extension
+    if ( in_array($extension_upload,$extensions_valides) ){
+        $erreur = "Extension valide";
+    } else {
+        $erreur = "Extension non-valide";
+    }
+
+    //Créer un identifiant difficile à deviner
+    $id = time(). md5(uniqid(rand(), true));
+
+    $nom = "fichier/".$id.".".$extension_upload;
+    $number = $id.".".$extension_upload;
+    $resultat = move_uploaded_file($_FILES['icone']['tmp_name'],$nom);
     
-        $extensions_valides = array( 'jpg' , 'jpeg' , 'gif' , 'png', 'ico' ,'mp3', 'm4a', 'ogg', 'wav', 'mp4', 'm4v', 'mov', 'wmv', 'zip', 'rar', 'avi', 'mpg', 'ogv', '3gp', '3g2', 'pdf', 'doc', 'docx', 'ppt', 'pptx', 'pps', 'ppsx', 'odt', 'xls', 'xlsx', 'psd' );
-        //1. strrchr renvoie l'extension avec le point (« . »).
-        //2. substr(chaine,1) ignore le premier caractère de chaine.
-        //3. strtolower met l'extension en minuscules.
-        $extension_upload = strtolower(  substr(  strrchr($_FILES['icone']['name'], '.')  ,1)  );
+    //Message de réussite
+    if ($resultat){
+        $info = "Transfert réussi !";
+    } else {
+        $info = "Ooops, Apparement quelque chose s'est mal passé...";
+    }
+    $passVar = [$extension_upload, $id, $number, $info, $erreur];
+    
+    return $passVar;
+}
+
+function updateDb($name, $extension_upload, $message, $id, $size, $date_up, $mailExpe, $expediteur, $mailDesti){
+
+    global $bdd; 
+
+    //Mise à jour de la base de donnée avec le nouveau fichier
+    require_once 'model/upload_model.php';
+    updateDbFile($name, $extension_upload, $message, $id, $size, $date_up);
+    updateDbSender($mailExpe, $expediteur);
+    updateDbReceiver($mailDesti);
+
+}
+
+function envoiMail($number, $emailExpediteur, $emailDestinataire, $message, $size, $fichier){
+
+    global $twig;
+    global $number;
+
+    if ($info = "Transfert réussi !") {
         
-        //Vérification de l'extension
-        if ( in_array($extension_upload,$extensions_valides) ){
-            $erreur = "Extension valide";
-        } else {
-            $erreur = "Extension non-valide";
-        }
-    
-        //Créer un identifiant difficile à deviner
-        $number = time(). md5(uniqid(rand(), true));
-    
-        $nom = "fichier/".$number.".".$extension_upload;
-        $resultat = move_uploaded_file($_FILES['icone']['tmp_name'],$nom);
+        $to = $emailDestinataire;
         
-        //Message de réussite
-        if ($resultat){
-            $info = "Transfert réussi !";
-        } else {
-            $info = "Ooops, Apparement quelque chose s'est mal passé...";
+        $subject = $emailExpediteur.' vous a envoyé des fichiers avec EzTransfer';
+        
+        $headers = 'MIME-Version: 1.0' . "\n";
+        
+        $headers .= 'Content-Type: text/html; charset=UTF-8' . "\n";
+        
+        function formatSize($size) {
+            if($size===false || $nbr===null) return '0 octet';
+            if($size>=1024*1024*1024*1024) return round($size/(1024*1024*1024*1024), 1)." ".('To');
+            if($size>=1024*1024*1024) return round($size/(1024*1024*1024), 1)." ".('Go');
+            if($size>=1024*1024) return round($size/(1024*1024), 1)." ".('Mo');
+            if($size>=1024) return round($size/(1024), 1)." ".('Ko');
+            if($size>=0) return intval($size)." ".('octets');
         }
-    };
+        
+        $msg = $twig->render('mail.twig', array('emailExpediteur'=>$emailExpediteur, 'emailDestinataire'=>$emailDestinataire, 'message'=>$message, 'size'=>$size, 'fichier'=>$fichier, 'number'=>$number));
+        $headers .= 'From: EzTransfer'."\n";
+        
+        mail($to, $subject, $msg, $headers);
 
-    function updateDb($name, $extension_upload, $message, $id, $size, $date_up, $mailExpe, $expediteur, $mailDesti){
-
-        //Mise à jour de la base de donnée avec le nouveau fichier
-       require_once ('model/upload_model.php');
-       updateDbFile($name, $extension_upload, $message, $id, $size, $date_up);
-       updateDbSender($mailExpe, $expediteur);
-       updateDbReceiver($mailDesti);
-
-   };
-
-   function envoiMail($number, $emailExpediteur, $emailDestinataire, $message, $size, $fichier, $submit){
-
-        global $twig;
-        global $number;
-
-        if ($info = "Transfert réussi !") {
-            
-            $to = $emailDestinataire;
-            
-            $subject = $emailExpediteur.' vous a envoyé des fichiers avec EzTransfer';
-            
-            $headers = 'MIME-Version: 1.0' . "\n";
-            
-            $headers .= 'Content-Type: text/html; charset=UTF-8' . "\n";
-            
-            function formatSize($size) {
-                if($size===false || $nbr===null) return '0 octet';
-                if($size>=1024*1024*1024*1024) return round($size/(1024*1024*1024*1024), 1)." ".('To');
-                if($size>=1024*1024*1024) return round($size/(1024*1024*1024), 1)." ".('Go');
-                if($size>=1024*1024) return round($size/(1024*1024), 1)." ".('Mo');
-                if($size>=1024) return round($size/(1024), 1)." ".('Ko');
-                if($size>=0) return intval($size)." ".('octets');
-            }
-            
-            $msg = $twig->render('mail.twig', array('emailExpediteur'=>$emailExpediteur, 'emailDestinataire'=>$emailDestinataire, 'message'=>$message, 'size'=>$size, 'fichier'=>$fichier, 'number'=>$number));
-            $headers .= 'From: EzTransfer'."\n";
-            
-            mail($to, $subject, $msg, $headers);
-
-        }
     }
 }
 
