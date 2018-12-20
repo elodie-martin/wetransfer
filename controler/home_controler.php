01c2e531887424c2ad3bc6cca2c80ee1c983abc8
vagrant <vagrant@vagrant.vm>
2018-12-20 11:13:33 +0000

    [home] Correction bug fonction upladod

--- controler/home_controler.php
+++ controler/home_controler.php
@@ -30,10 +30,9 @@ switch ($action) {
 
 //Fonction d'upload du fichier
 function upload(){
-
-    global $bdd;
+    
     global $twig;
-
+    
     //Récupération des données du formulaire
     $name = $_FILES['icone']['name'];     //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
     $type = $_FILES['icone']['type'];     //Le type du fichier. Par exemple, cela peut être « image/png ».
@@ -50,7 +49,7 @@ function upload(){
 
     $upvars = uploadFile($name, $type, $size, $tmp_name, $error, $maxsize);   //Upload du fichier sur le serveur
     
-    //Variabes à récupérer de la fonction uploadFile et à passer aux fonctions suivantes
+    // //Variabes à récupérer de la fonction uploadFile et à passer aux fonctions suivantes
     $extension_upload = $upvars[0]; 
     $id = $upvars[1];
     $number = $upvars[2];
@@ -58,11 +57,11 @@ function upload(){
     $erreur = $upvars[4];
     $titre = $upvars[5];
 
-    //echo 'variables bdd : '.$name." : ".$extension_upload." : ".$message." : ".$id." : ".$size." : ".$date_up." : ".$mailExpe." : ".$expediteur." : ".$mailDesti;
+    // //echo 'variables bdd : '.$name." : ".$extension_upload." : ".$message." : ".$id." : ".$size." : ".$date_up." : ".$mailExpe." : ".$expediteur." : ".$mailDesti;
     updateDb($name, $extension_upload, $message, $id, $size,  $date_up, $mailExpe, $expediteur, $mailDesti);     //Mise à jour de la base de données
     envoiMail($info, $number, $mailExpe, $mailDesti, $message, $size, $name);    //Envoie du mail
 
-    //affichage de la page d'information
+    // //affichage de la page d'information
     echo $twig->render('info.twig', array('titre'=>$titre, 'info'=>$info, 'nom_fichier'=>$name, "poids"=>$size, 'expediteur'=>$expediteur, 'message'=>$message, 'erreur'=>$erreur));
 
   
@@ -114,51 +113,14 @@ function uploadFile($name, $type, $size, $tmp_name, $error, $maxsize){
 
 function updateDb($name, $extension_upload, $message, $id, $size, $date_up, $mailExpe, $expediteur, $mailDesti){
 
-    global $bdd; 
-
-//Récupération des données du formulaire
-$name = $_FILES['icone']['name'];     //Le nom original du fichier, comme sur le disque du visiteur (exemple : mon_icone.png).
-$type = $_FILES['icone']['type'];     //Le type du fichier. Par exemple, cela peut être « image/png ».
-$size = $_FILES['icone']['size'];     //La taille du fichier en octets.
-$tmp_name = $_FILES['icone']['tmp_name']; //L'adresse vers le fichier uploadé dans le répertoire temporaire.
-$error = $_FILES['icone']['error'];    //Le code d'erreur, qui permet de savoir si le fichier a bien été uploadé.
-$maxsize = $_POST["MAX_FILE_SIZE"]; //Taille maximum des fichiers
-$message = $_POST['message'];       //Message laissé par l'éxpéditeur
-
-
-$file = $_SERVER["DOCUMENT_ROOT"]."/fichier/$idFile";
-$error = false;
-
-if (file_exists($file)) {
-    header('Content-Description: File Transfer');
-    header('Content-Type: application/octet-stream');
-    header('Content-Disposition: attachment; filename='.basename($file));
-    header('Content-Transfer-Encoding: binary');
-    header('Expires: 0');
-    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
-    header('Pragma: public');
-    header('Content-Length: ' . filesize($file));
-    ob_clean();
-    flush();
-    readfile($file);
-    exit;
-    } else {
-        $error=true;
-    }
+    global $bdd;
+    
 
     //Mise à jour de la base de donnée avec le nouveau fichier
-    require_once ('model/download_model.php');
-    getInfoDownload($number[0]);
-    $number = explode(".", trim($idFile, '.'));
+    require_once ('model/upload_model.php');
     updateDbFile($name, $extension_upload, $message, $id, $size, $date_up);
     updateDbSender($mailExpe, $expediteur);
-    updateDbReceiver($mailDesti);
-    
-
-    //affichage de la page d'information
-    echo $twig->render('download.twig', array('info'=>$info, 'erreur'=>$erreur));
-    
-
+    updateDbReceiver($mailDesti); 
 
 }
 
